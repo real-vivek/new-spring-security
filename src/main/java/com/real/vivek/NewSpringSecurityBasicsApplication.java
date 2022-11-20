5175683aad37f09841a6b6bff0cd28cfe33b6be3
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2022-11-20 19:56:05 +0530

    Changes to see builtin filters provided by Spring Security

--- src/main/java/com/real/vivek/NewSpringSecurityBasicsApplication.java
+++ src/main/java/com/real/vivek/NewSpringSecurityBasicsApplication.java
@@ -2,7 +2,10 @@ package com.real.vivek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+//This annotation will start spring security in debug mode
+@EnableWebSecurity(debug=true)
 @SpringBootApplication
 public class NewSpringSecurityBasicsApplication {
 
