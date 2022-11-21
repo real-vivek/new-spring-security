4153172afcbf3e8d5e42848691863d34ae54d74a
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2022-11-21 22:41:59 +0530

    Fix to make role based authentication work along with authority based authentication

--- src/main/java/com/real/vivek/NewSpringSecurityBasicsApplication.java
+++ src/main/java/com/real/vivek/NewSpringSecurityBasicsApplication.java
@@ -2,10 +2,9 @@ package com.real.vivek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 //This annotation will start spring security in debug mode
-@EnableWebSecurity(debug=true)
+//@EnableWebSecurity(debug=true)
 @SpringBootApplication
 public class NewSpringSecurityBasicsApplication {
 
--- src/main/java/com/real/vivek/config/CustomAuthenticationProvider.java
+++ src/main/java/com/real/vivek/config/CustomAuthenticationProvider.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -12,6 +14,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -28,32 +32,61 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private MultiTableDetailsService multiTableDetailsService;
+
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String username = authentication.getName();
 		String pwd = authentication.getCredentials().toString();
-		List<Customer> customers = customerRepository.findByEmail(username);
-		if (customers.size() > 0) {
-			if (passwordEncoder.matches(pwd, customers.get(0).getPwd())) {
-				return new UsernamePasswordAuthenticationToken(username, pwd, getGrantedAuthorities(customers.get(0).getAuthorities()));
-			}else {
-				throw new BadCredentialsException("Invalid password");
+
+		// Logic to load users from customerRepository and getAuthorities
+		if (username.contains("@")) {
+			List<Customer> customers = customerRepository.findByEmail(username);
+			if (customers.size() > 0) {
+				if (passwordEncoder.matches(pwd, customers.get(0).getPwd())) {
+					return new UsernamePasswordAuthenticationToken(username, pwd,
+							getGrantedAuthorities(customers.get(0).getAuthorities()));
+				} else {
+					throw new BadCredentialsException("Invalid password");
+				}
+			} else {
+				throw new BadCredentialsException("No user registered with this details!");
 			}
-		}else {
-			throw new BadCredentialsException("No user registered with this details!");
 		}
+		// Logic to load in-memory users and getAuthorities
+		else {
+			UserDetailsService defaultInMemoryUserDetailsService = multiTableDetailsService.jdbcDaoImpl(dataSource);
+			UserDetails inMemoryUser = defaultInMemoryUserDetailsService.loadUserByUsername(username);
+			if (inMemoryUser != null) {
+				if (passwordEncoder.matches(pwd, inMemoryUser.getPassword())) {
+					return new UsernamePasswordAuthenticationToken(username, pwd, inMemoryUser.getAuthorities());
+				} else {
+					throw new BadCredentialsException("Invalid password");
+				}
+			} else {
+				throw new BadCredentialsException("No user registered with this details!");
+			}
+
+		}
+
 	}
 
 	@Override
 	public boolean supports(Class<?> authentication) {
 		return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
 	}
-	
-	//If we pass set of authorities from backend db, this helper method will read each authorities from this set and then create object of SimpleGrantedAuthority by passing name of authority inside it's constructor 
-	private List<GrantedAuthority> getGrantedAuthorities(Set<Authority> authorities){
+
+	// If we pass set of authorities from backend db, this helper method will read
+	// each authorities from this set and then create object of
+	// SimpleGrantedAuthority by passing name of authority inside it's constructor
+	private List<GrantedAuthority> getGrantedAuthorities(Set<Authority> authorities) {
 		List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
-		for (Authority authority: authorities) {
-			grantedAuthorities.add(new SimpleGrantedAuthority(authority.getAuthorityName()));		
+		for (Authority authority : authorities) {
+			grantedAuthorities.add(new SimpleGrantedAuthority(authority.getAuthorityName()));
 		}
 		return grantedAuthorities;
 	}
